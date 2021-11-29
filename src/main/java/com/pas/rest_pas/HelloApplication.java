2f20c6deb8125ea6625295d76a39814ceb8dd78e
230049 <230049@edu.p.lodz.pl>
2021-11-29 17:42:47 +0100

    Another commit

--- src/main/java/com/pas/rest_pas/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package com.pas.rest_pas;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class HelloApplication extends Application {
-
-}
---
+++ src/main/java/com/pas/rest_pas/entities/User.java
@@ -0,0 +1,14 @@
+package com.pas.rest_pas.entities;
+
+import java.util.UUID;
+
+public class User extends Entity{
+
+
+    private boolean isActive;
+    private String login;
+    private String password;
+
+    public User() {
+    }
+}
