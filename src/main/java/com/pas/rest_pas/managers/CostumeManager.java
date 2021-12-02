9bcba063882c89832481f5b37d943646b893c0dc
Kamila Topolska <kamiira97@gmail.com>
2021-12-02 11:15:05 +0100

    jazda

--- src/main/java/com/pas/rest_pas/managers/CostumeManager.java
+++ src/main/java/com/pas/rest_pas/managers/CostumeManager.java
@@ -2,6 +2,7 @@ package com.pas.rest_pas.managers;
 import com.pas.rest_pas.dao.CostumeRepository;
 import com.pas.rest_pas.entities.Costume;
 
+import java.util.List;
 import java.util.UUID;
 
 public class CostumeManager {
@@ -12,21 +13,16 @@ public class CostumeManager {
         this.costumeRepository = costumeRepository;
     }
 
-//    public boolean add(Costume costume) {
-//        if (!userRepository.add(user)) return false;
-//        return true;
-//    }
-//
-//    public User getUserById(UUID id) {
-//        return userRepository.getById(id);
-//    }
-//
-//    public User getUserByLogin(String login) {
-//        return userRepository.getUserByLogin(login);
-//    }
-//
-//    public void getAll() {
-//        userRepository.getAll();
-//    }
+    public boolean add(Costume costume) {
+        return costumeRepository.add(costume);
+    }
+
+    public List<Costume> getCostumesBySize(int size) {
+        return costumeRepository.getCostumesBySize(size);
+    }
+
+    public void getAll() {
+        costumeRepository.getAll();
+    }
 
 }
