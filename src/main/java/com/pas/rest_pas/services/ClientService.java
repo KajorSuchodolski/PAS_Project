0f18c2ec205e2c4291b15e9aab5d6046fb981ac7
230049 <230049@edu.p.lodz.pl>
2021-12-02 11:12:39 +0100

    Another task is coming

--- src/main/java/com/pas/rest_pas/services/ClientService.java
+++ src/main/java/com/pas/rest_pas/services/ClientService.java
@@ -1,15 +1,14 @@
 package com.pas.rest_pas.services;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.core.Response;
 
-//@Path("/client")
-//public class ClientService {
-////    @GET
-////    @Path("{login}")
-////    public String getClientByLogin( @PathParam("login") String login) {
-////        return Response.g
-////    }
-//}
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/clientservice")
+public class ClientService {
+
+//    @GET
+//    @Path("{login}")
+//    public String getClientByLogin( @PathParam("login") String login) {
+//        return Response.g
+//    }
+}
