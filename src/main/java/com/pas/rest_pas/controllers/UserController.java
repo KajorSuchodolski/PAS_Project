2a1e756ea56369be75d4d6f0d280f81bde77300c
230049 <230049@edu.p.lodz.pl>
2021-12-04 00:56:48 +0100

    Gówno z dupy

--- src/main/java/com/pas/rest_pas/controllers/UserController.java
+++ src/main/java/com/pas/rest_pas/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.pas.rest_pas.controllers;
 
 
+import com.pas.rest_pas.entities.access_levels.AdministatorLevel;
 import com.pas.rest_pas.entities.user.User;
 import com.pas.rest_pas.managers.UserManager;
 
@@ -11,13 +12,15 @@ import javax.ws.rs.client.Client;
 @Path("/clients")
 public class UserController {
 
-    @Inject
     private UserManager userManager;
 
     @GET
     @Produces("application/json")
     @Path("/find/{login}")
-    public User getUserByLogin( @PathParam("login") String login) {
+
+        public User getUserByLogin( @PathParam("login") String login) {
         return userManager.getUserByLogin("login");
     }
+
+
 }
